2bc178334aa1bb724febf940e1395cb1813b14e9
kchasteSun <kchaste@163.com>
2022-11-17 18:15:44 +0800

    init project

---
+++ my-spring/src/test/java/com/yck/FirstTest.java
@@ -0,0 +1,16 @@
+package com.yck;
+
+import com.yck.entity.User;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+	@Test
+	public void Test(){
+
+		/*BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring-config.xml"));
+		User user = (User) beanFactory.getBean("user");
+		System.out.println(user);*/
+	}
+
+}
